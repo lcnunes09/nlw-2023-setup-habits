08ec9b0d85065a458e9795256e244457b4bec079
Luciana Nunes <lcnunes09@gmail.com>
2023-01-21 20:50:56 -0600

    feat: Add routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,11 @@
 import Fastify from 'fastify'
 import cors from '@fastify/cors'
-//import { appRoutes } from './routes'
+import { appRoutes } from './routes'
 
 const app = Fastify()
 
 app.register(cors)
-//app.register(appRoutes)
+app.register(appRoutes)
 
 app.listen({
     port: 3333,
