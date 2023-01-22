033c09751d868f40ddc1c1fd67e10873ed443571
Luciana Nunes <lcnunes09@gmail.com>
2023-01-21 19:45:38 -0600

    feat: Add habit detail page mobile

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,4 +1,5 @@
 import { View, Text, ScrollView } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 import { generateDatesFromYearBeginning } from '../utils/generate-dates-from-year-beginning';
 
@@ -11,6 +12,9 @@ const minimumSummaryDatesSizes = 18 * 5;
 const amountOfDaysToFill = minimumSummaryDatesSizes - datesFromYearBeginnin.length;
 
 export function Home() {
+
+    const { navigate } = useNavigation()
+
     return (
         <View className="flex-1 bg-background px-8 pt-16">
             <Header />
@@ -39,6 +43,7 @@ export function Home() {
                         datesFromYearBeginnin.map(date => (
                             <HabitDay
                                 key={date.toISOString()}
+                                onPress={() => navigate('habit', { date: date.toISOString() })}
                             />
                         ))
                     }
