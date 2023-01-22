6cc446142128b1147a0e3910a6dfd4e9393bfa55
Luciana Nunes <lcnunes09@gmail.com>
2023-01-21 19:19:13 -0600

    feat: Add navigation mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,8 +1,9 @@
 import './src/lib/dayjs';
 
 import { StatusBar } from 'react-native';
+
 import { Loading } from './src/components/Loading';
-import { Home } from './src/screens/Home';
+import { Routes } from './src/routes';
 
 import { 
   useFonts,
@@ -28,7 +29,7 @@ export default function App() {
 
   return (
     <>
-      <Home />
+      <Routes />
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent/>
     </>
   )
--- mobile/src/components/Header.tsx
+++ mobile/src/components/Header.tsx
@@ -1,19 +1,22 @@
-import { TouchableOpacity, View, Text } from "react-native";
-import { Feather } from "@expo/vector-icons";
+import { TouchableOpacity, View, Text } from "react-native"
+import { Feather } from "@expo/vector-icons"
+import { useNavigation } from "@react-navigation/native"
 
-import colors from 'tailwindcss/colors';
+import colors from 'tailwindcss/colors'
 
-import Logo from '../assets/logo.svg';
+import Logo from '../assets/logo.svg'
 
 export function Header() {
+    const { navigate } = useNavigation()
+
     return (
         <View className="w-full flex-row items-center justify-between">
-            <Logo />
+           <Logo />
 
             <TouchableOpacity
                 activeOpacity={0.7}
                 className="flex-row h-11 px-4 border border-violet-500 rounded-lg items-center"
-                
+                onPress={() => navigate('new')}
             >
 
                 <Feather
